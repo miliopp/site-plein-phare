027ecfc1c56aa6c13909d482ce5ee3773a34a613
Emilien Jouando <emilien.jouando@gmail.com>
2025-04-17 11:20:17 +0200

    correction

--- app/artists/[id]/page.tsx
+++ app/artists/[id]/page.tsx
@@ -2,8 +2,9 @@ import { notFound } from 'next/navigation';
 import artists from '../../data/artists.json';
 import ArtistDetail from './ArtistDetail';
 
-export default async function ArtistPage({ params }: { params: { id: string } }) {
-  const artist = artists.find((a) => a.id === params.id);
+export default async function ArtistPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const artist = artists.find((a) => a.id === id);
   if (!artist) notFound();
 
   return <ArtistDetail artist={artist} />;
